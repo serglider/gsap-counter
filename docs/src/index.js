7b53ddb5ec6b1cf78a404318364feb29e02db7ae
serglider <s-chernykh@outlook.com>
2022-02-07 06:17:51 +0200

    dom demo fix

--- docs/src/index.js
+++ docs/src/index.js
@@ -4,7 +4,7 @@ import { createControls } from './Controls';
 import { createPixiApp } from './PixiApp';
 import { createHTMLApp } from './HTMLApp';
 
-const searchParams = new URLSearchParams(document.location.href);
+const searchParams = new URLSearchParams(document.location.search);
 const renderer = searchParams.has('dom') ? createHTMLApp : createPixiApp;
 const counter = new Counter(gsap.to);
 // counter.setDecimalsEpsilon(0.27);
