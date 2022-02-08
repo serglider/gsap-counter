ed54ca09510bb6b3f3a3d519d6f5c96d2716a516
serglider <s-chernykh@outlook.com>
2022-02-08 18:50:19 +0200

    eslint test 7

--- docs/src/HTMLApp.js
+++ docs/src/HTMLApp.js
@@ -1,10 +1,10 @@
 import { financial } from './format';
 
 export function createHTMLApp(counter) {
-    const counterBlock = document.getElementById('counter');
-    const tfLayout = `<div id="tf">$0.00</div>`;
+    const counterBlock =   document.getElementById('counter');
+    const tfLayout = '<div id="tf">$0.00</div>';
     counterBlock.insertAdjacentHTML('afterbegin', tfLayout);
-    const tf = counterBlock.querySelector('#tf');
+    const tf =   counterBlock.querySelector('#tf');
 
     function updateTF(val) {
         tf.textContent = financial(val);
